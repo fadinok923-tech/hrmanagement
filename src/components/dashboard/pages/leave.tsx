f500b63f5cf1111173acb16c1e1f9b2c9449c35d
fadinok923-tech <fadinok923-tech@users.noreply.github.com>
2026-07-13 08:50:40 +0300

    Add casual leave support

--- src/components/dashboard/pages/leave.tsx
+++ src/components/dashboard/pages/leave.tsx
@@ -9,7 +9,7 @@ import { DataTable, type Column } from "../data-table";
 import { ConfirmDialog } from "../confirm-dialog";
 import { normalizeLeaveList, type NormalLeave } from "../api-helpers";
 
-const TYPES = ["annual", "sick", "emergency", "unpaid", "maternity"];
+const TYPES = ["annual", "sick", "emergency", "unpaid", "maternity", "casual"];
 
 export function LeavePage() {
   const { t } = useLanguage();
